fdda7a17abf5c4a26de19f91beb12b81bfac6bde
ElisaResminiT4U <elisa.resmini@training4u.it>
2025-10-15 10:34:32 +0200

    deploy vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import { router } from "./router/routers";
 
+
 const app = createApp(App);
 app.use(ElementPlus);
 app.use(router);
